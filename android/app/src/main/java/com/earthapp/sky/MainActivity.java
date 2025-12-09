d2094e402a59601a428176fadc828fbd2b065dcf
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-12-09 14:39:38 -0600

    feat: add native files

---
+++ android/app/src/main/java/com/earthapp/sky/MainActivity.java
@@ -0,0 +1,5 @@
+package com.earthapp.sky;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ scripts/dev-android.sh
@@ -0,0 +1,9 @@
+LIP=$(ipconfig getifaddr en0)
+
+echo "🍦 Starting local development to android device - ensure local dev server is running already"
+echo "🏗️ Type checking and building for development..."
+bunx run build:dev
+echo "🔃 Capacitor installation, podfile installation, sync and copy to app distribution folders..."
+bunx ionic capacitor sync android --no-build
+echo "🏃 Select an Android device to run the build at local ip address ${LIP} on..."
+eval "bunx ionic capacitor run android --livereload-url=http://${LIP}:3000  --external --mode development"
