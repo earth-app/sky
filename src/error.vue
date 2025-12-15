c7c0caca3c80a82f72e6bb202745a34a5c541256
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-12-14 20:36:51 -0600

    feat: add error page

---
+++ src/error.vue
@@ -0,0 +1,29 @@
+<template>
+	<IonApp>
+		<div class="flex flex-col items-center py-24 min-h-screen">
+			<IonImg
+				src="/cloud.png"
+				alt="Error Image"
+				format="webp"
+				class="size-48 max-w-2xs max-h-2xs mb-6"
+				loading="eager"
+				decoding="async"
+				fetchpriority="high"
+			/>
+			<h1 class="text-4xl font-bold mb-4">Oops! Something went wrong.</h1>
+			<p class="text-lg mb-8">
+				{{ error?.message || "We're sorry, but we couldn't find the page you were looking for." }}
+			</p>
+			<IonButton
+				class="hover:cursor-pointer"
+				@click="$router.push('/')"
+			>
+				Go Back Home
+			</IonButton>
+		</div>
+	</IonApp>
+</template>
+
+<script setup lang="ts">
+const error = useError();
+</script>
