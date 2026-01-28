4a08ec181e288bc51b65e5e7205b0e1b6c27fa9a
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2026-01-28 14:52:46 -0600

    build: exclude repetitive build in dev task

--- scripts/dev-android.sh
+++ scripts/dev-android.sh
@@ -1,9 +1,5 @@
-LIP=$(ipconfig getifaddr en0)
-
-echo "🍦 Starting local development to android device - ensure local dev server is running already"
-echo "🏗️ Type checking and building for development..."
-bun run generate:android
+echo "🍦 Starting local development to android device"
 echo "🔃 Capacitor installation, podfile installation, sync and copy to app distribution folders..."
 bunx ionic capacitor sync android --no-build
-echo "🏃 Select an Android device to run the build at local ip address ${LIP} on..."
-eval "bunx ionic capacitor run android --livereload-url=http://${LIP}:3001  --external --mode development"
+echo "🏃 Select an Android device to run the build..."
+eval "bunx ionic capacitor run android --external --mode development --no-build"
--- scripts/dev-ios.sh
+++ scripts/dev-ios.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-LIP=$(ipconfig getifaddr en0)
-
-echo "🍦 Starting local development to ios device - ensure local dev server is running already"
-echo "🏗️ Type checking and building for development..."
-bun run generate:ios
+echo "🍦 Starting local development to ios device"
 echo "🔃 Capacitor installation, podfile installation, sync and copy to app distribution folders..."
 bunx ionic capacitor sync ios --no-build
-echo "🏃 Select an iOS device to run the build at local ip address ${LIP} on..."
-eval "bunx ionic capacitor run ios --livereload-url=http://${LIP}:3001 --external --mode development --no-build"
+echo "🏃 Select an iOS device to run the build..."
+eval "bunx ionic capacitor run ios --external --mode development --no-build"
