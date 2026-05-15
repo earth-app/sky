d263ab99e57a2914993342c42c6c2c61e5044680
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2026-05-15 18:54:53 -0500

    feat: create dedicated ranks page

---
+++ src/pages/tabs/upgrade.vue
@@ -0,0 +1,28 @@
+<template>
+	<IonPage>
+		<IonHeader>
+			<IonToolbar>
+				<IonTitle>Upgrade Account</IonTitle>
+				<IonButtons slot="start">
+					<IonBackButton default-href="/tabs/dashboard" />
+				</IonButtons>
+			</IonToolbar>
+		</IonHeader>
+		<IonContent :scroll-y="true">
+			<MRanks :highlighted="highlighted" />
+		</IonContent>
+	</IonPage>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+
+const highlighted = computed(() => {
+	const param = route.query.highlighted;
+	if (param) {
+		return param.toString().toUpperCase() as 'WRITER' | 'ORGANIZER' | 'PRO';
+	}
+
+	return undefined;
+});
+</script>
