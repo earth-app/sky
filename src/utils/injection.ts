4383f80464d6b28d2437fe76e0c8ef6220357ee6
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2026-06-02 03:00:48 -0500

    fix: badges drawer close injection

--- src/utils/injection.ts
+++ src/utils/injection.ts
@@ -1,3 +1,3 @@
 import type { InjectionKey } from 'vue';
 
-export const BADGES_DRAWER_CLOSE: InjectionKey<() => void> = Symbol('badges-drawer-close');
+export const BADGES_DRAWER_CLOSE: InjectionKey<() => Promise<void>> = Symbol('badges-drawer-close');
